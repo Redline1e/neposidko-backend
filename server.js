42e6169897a8ed67081dde927a5d72ed92540ee5
Redline1e <ahorobiichenko@gmail.com>
2025-04-23 21:39:30 +0300

    Minor changes

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express();
 // Налаштування CORS
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONT_END_URL,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
